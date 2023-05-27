ed14c51b0114dee2efd1e51574d37ff16ceeb425
Philipp Wagner <info@converter.app>
2023-05-27 15:35:49 +0200

    added routes to the manifest

--- api/main.py
+++ api/main.py
@@ -19,6 +19,18 @@ def hello_world():
     data = {d['label']:d['url'] for d in response}
     
     return app.response_class(response=json.dumps(data), status=200)
-         
+
+@app.route('/.well-known/ai-plugin.json', methods=['GET', 'OPTIONS'])
+def get_plugin_json():
+    return send_from_directory("./.well-known", 'ai-plugin.json', mimetype='application/json')
+
+@app.route('/openapi.yaml', methods=['GET', 'OPTIONS'])
+def get_openapi_yaml():
+    return send_from_directory('.', 'openapi.yaml', mimetype='text/yaml')
+
+@app.route('/logo.png', methods=['GET', 'OPTIONS'])
+def plugin_logo():
+    return send_from_directory('.', 'logo.png', mimetype='image/png')
+
 if __name__ == "__main__":
     app.run(debug=True)
